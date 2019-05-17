0f1adbc7a8e548f79c813c5b2dfd6f1acdbc7583
Braian Santos <bst0897@gmail.com>
2019-05-17 10:41:17 -0400

    Ultimos cambios a excepcion de peticion

--- Practica1/src/main/java/Main.java
+++ Practica1/src/main/java/Main.java
@@ -8,15 +8,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Favor digite una URL valida:/n");
+        System.out.println("Favor digite una URL valida:");
         Scanner input = new Scanner(System.in);
         String url =input.nextLine();
-        System.out.println(url);
+        //System.out.println(url);
 
         Document doc = Jsoup.connect(url).get();
+
         log(doc.title());
 
         Elements newsHeadlines = doc.select("#mp-itn b a");
+        Elements pngs = doc.select("img");
+        Elements parrafos = doc.select("p");
+
+        System.out.println("Numero de imagenes : " + pngs.size());
+        System.out.println("Numero de Parrafos : " + parrafos.size());
         for (Element headline : newsHeadlines) {
             log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
         }
